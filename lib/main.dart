37f90af670a5be83d0d47cb813ff40d58ca5d213
azlouk96 <ahmedazlouk9@gmail.com>
2024-08-05 09:04:02 +0100

    containers

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,17 @@ class SimpleWidget extends StatelessWidget {
           IconButton(
             onPressed: () {},
             icon: Icon(
-              Icons.menu,
-              size: 30,
+              Icons.message,
+              size: 20,
+              color: Color.fromRGBO(
+                  28, 104, 255, 1), 
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications,
+              size: 20,
               color: Color.fromRGBO(
                   28, 104, 255, 1), // Adjusted alpha value to 255
             ),
@@ -33,46 +42,40 @@ class SimpleWidget extends StatelessWidget {
           IconButton(
             onPressed: () {},
             icon: Icon(
-              Icons.menu,
-              size: 30,
+              Icons.verified_user_sharp,
+              size: 20,
               color: Color.fromRGBO(
                   28, 104, 255, 1), // Adjusted alpha value to 255
             ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.menu,
-              size: 30,
-              color: Color.fromRGBO(
-                  28, 104, 255, 1), // Adjusted alpha value to 255
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.menu,
-              size: 30,
-              color: Color.fromRGBO(
-                  28, 104, 255, 1), // Adjusted alpha value to 255
-            ),
-          ),
+          )
         ],
         elevation: 10,
         backgroundColor: Color.fromARGB(28, 1, 162, 255),
         title: Text("facebook"),
-        centerTitle: true,
         leading: IconButton(
           onPressed: () {},
           icon: Icon(
             Icons.menu,
-            size: 30,
+            size: 20,
             color:
                 Color.fromRGBO(28, 104, 255, 1), // Adjusted alpha value to 255
           ),
         ),
       ),
-      body: Text("body of my widget" ,style: TextStyle(color: Color.fromARGB(255, 7, 169, 178),fontSize: 33)),
+      body: Container(
+        child: Text("body of my widget"),
+        height: 200,
+        width: 200,
+        
+        alignment: Alignment.center,
+        margin: EdgeInsets.all(20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Color.fromARGB(28, 1, 162, 255),
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+      floatingActionButton: IconButton(onPressed: ()=>{}, icon: Icon(Icons.add),),
     );
   }
 }
